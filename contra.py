ebf1a99a365ee5641cec30e294be06238406f731
KairiyamaCristian <cristiankairiyama@gmail.com>
2022-09-15 15:19:18 -0400

    incorporacion de palabras nuevas

--- contra.py
+++ contra.py
@@ -1,27 +1,50 @@
 import random
 
+#function que devuelve un string_random a partir de una lista
+def limitar_random(num,lista_caracter):
+    list_random = []
+    for i in range(num):
+        caracter_random = random.choice(lista_caracter)
+        list_random.append(caracter_random)
+    return list_random
+
+
+
 def generar_contrasena():
 
-    #Separamos las partes que incorporara la contraseña
-    palabras = ['case', 'small', 'not', 'yesterday', 'now']
-    palabrasespanol = ['casa', 'rata', 'mamifero', 'perro']
-    numeros = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
-    simbolos =['!', '-', "$", "&"]
+    #Separamos los components
+    lista_mayuscula= ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    lista_minuscula= ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    lista_numeros= ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+    lista_simbolos= ['!', '&', '$', '(', ')', '?']
+
+    #creamos void list
+    mayusculas = []
+    minusculas = []
+    numeros = []
+    simbolos = []
+    palabras = []
+
+    #restringimos cantidad
+    mayusculas = limitar_random(5,lista_mayuscula)
+    minusculas = limitar_random(0,lista_minuscula)
+    numeros = limitar_random(5,lista_numeros)
+    simbolos = limitar_random(6,lista_simbolos)
+
+    palab = input('Introduce la palabra que quieras agregar: ')
 
     #introducir nuevas palabras con la contrasena
+    while( palab!= '0' ):
+        palabras.append(palab)
+        palab = input('Introduce otra palabra que quieras agregar: ')
 
     #metemos en una misma lista (mutabilidad)
-    caracteres= simbolos + numeros
+    caracteres = simbolos + numeros + mayusculas + minusculas + palabras
 
-    #creamos una lista vacia "contraseña"
-    contrasena= []
-
-    #Ponemos "5" caracteres random
-    for i in range(5):
-        caracter_random = random.choice(caracteres)
-        contrasena.append(caracter_random)
+    contrasena = limitar_random(5,caracteres)
     contrasena = "".join(contrasena)
     return contrasena
+
 
 def run():
     contrasena = generar_contrasena()
